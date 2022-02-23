7c5ec87365320a4adca4b776458d951642373e99
Andre LaFleur <cincospenguinos@gmail.com>
2022-02-22 17:56:21 -0700

    Extract a class

---
+++ app/javascript/packs/Find/config/credits.js
@@ -0,0 +1,8 @@
+const CREDITS = [{
+	title: "FisherG's City Tiles",
+	usedFor: 'Virtually everything',
+	message: "Thank you, FisherG! I couldn't make this without your work!",
+	url: "https://opengameart.org/content/12x12-city-tiles-top-down",
+}];
+
+export { CREDITS };
--- app/javascript/packs/Find/scenes/GameScene.js
+++ app/javascript/packs/Find/scenes/GameScene.js
@@ -1,12 +1,21 @@
 import Phaser from 'phaser';
 import { CONFIG } from '../config/index.js';
 
+class PlayerSprite extends Phaser.GameObjects.Sprite {
+	constructor(scene, info) {
+		super(scene, info.x, info.y, 'player');
+		scene.add.existing(this);
+		scene.anims.createFromAseprite('player');
+	}
+}
+
 export default class GameScene extends Phaser.Scene {
 	constructor() {
 		super({ key: 'GameScene' });
 	}
 
 	init(data) {
+		// TODO: Startup data will have all of the information necessary for the game. Use it to set the player's initial information
 		this._startupData = {
 			sprite: CONFIG.sprites.player1,
 			...data
@@ -19,12 +28,10 @@ export default class GameScene extends Phaser.Scene {
 
 	create() {
 		this.cameras.main.setBackgroundColor('#FFFFFF');
-		this.player = this.add.sprite(300, 300, 'player');
+		this.player = new PlayerSprite(this, { x: 150, y: 150 });
 
 		const playerAnims = this.anims.createFromAseprite('player');
 	}
 
-	update() {
-
-	}
+	update() {}
 }
